4d7c933c437d94dbf031122b3436454123939304
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-08-31 00:33:03 +0200

    fixing login, password resert and much more

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def health_check():
 app.include_router(social.router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
